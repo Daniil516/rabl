047a5943769945d934a6094f1b25e84713ee8d53
GitHub Action <action@github.com>
2023-03-26 16:01:37 +0000

    Template cleanup

--- src/main/kotlin/com/github/daniil516/rabl/services/MyProjectService.kt
+++ src/main/kotlin/com/github/daniil516/rabl/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.daniil516.rabl.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.daniil516.rabl.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
